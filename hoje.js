d82c848b37101a5dedadc6ebaf749d1fea117f19
cletche <61995271+cletche1@users.noreply.github.com>
2020-05-12 21:44:41 -0300

    Add files via upload

---
+++ hoje.js
@@ -0,0 +1,25 @@
+const inputnovaTarefaInput = document.querySelector("#new-task")
+const botaoAdicionar = document.querySelector("#add-task-button")
+const listaDeTarefas = document.querySelector("task-list")
+
+function adicionarTarefa(event){
+
+ const novoItem = document.createElement("li")
+
+ novoItem.textContent = novaTarefaInput.value
+ novoItem.classList.add("task-list__item")
+ 
+
+
+
+
+
+
+
+
+}
+
+
+
+
+button.adicionarTarefa("click", adicionarTarefa)
---
+++ index.js
@@ -0,0 +1,16 @@
+
+function incluir(){
+
+    var add= document.querySelector("#new-task")
+    var tasklist= document.querySelector(".task-list")
+    var liNode= document.createElement("li")
+    var txtNode = document.createTextNode(name)
+
+    liNode.appendChild(txtNode);
+    entrada.appendChild(liNode);
+
+
+ 
+
+
+}
